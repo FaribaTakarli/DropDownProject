9be0b002f0cf7729a89c42c302acc707fe176dc6
YOUR_USERNAME <fa.takarli@gmail.com>
2022-11-21 13:45:40 +0330

    Add comment to dropdown

--- src/components/dropDown.jsx
+++ src/components/dropDown.jsx
@@ -95,3 +95,12 @@ function updateShowUl()
 
 
 export default DropDown;
+
+
+//  const CreateUniqueList = (StudentIds) => {
+//      //let StudentIds = [1, 2, 3, 3, 3, 4, 4, 5, 5, 6];
+//      let UnqueStudentIds = [...new Set(StudentIds)];
+//      return UnqueStudentIds;
+//  };
+
+//  export default CreateUniqueList;
